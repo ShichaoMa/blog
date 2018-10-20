a2c49305b67ae3db7340b6ff4647dec4583659ec
马式超 <shichao.ma@yiducloud.cn>
2018-10-20 22:15:09 +0800

    Feature: 增加api

--- blog/blog/welcome/__init__.py
+++ blog/blog/welcome/__init__.py
@@ -1,9 +1,10 @@
 import os
+import glob
 
 from apistar import App
 from apistar.http import QueryParam
 
-from apistellar import Controller, route, get, post, require, Session, FormParam
+from apistellar import Controller, route, get, post, require, Session, FormParam, FileStream
 from toolkit.settings import FrozenSettings
 
 from ..utils import project_path
@@ -27,3 +28,30 @@ class WelcomeController(Controller):
     def upload(file: FormParam):
         file.save(os.path.join(project_path, "static/img", file.filename))
         return {"success": True}
+
+    @post("/upload_secret")
+    @require(Session, judge=lambda x: x.get("login"))
+    async def upload_secret(stream: FileStream):
+        file_dir = os.path.join(project_path, "static/secret")
+        os.makedirs(file_dir, exist_ok=True)
+        async for file in stream:
+            if file.filename:
+                with open(os.path.join(
+                        file_dir, file.filename.replace(" ", "")), "wb") as f:
+                    async for chuck in file:
+                        f.write(chuck)
+        return {"success": True}
+
+    @get("/list")
+    @require(Session, judge=lambda x: x.get("login"))
+    def list(self: Controller):
+         file_dir = os.path.join(project_path, "static/secret")
+         html_tmpl = "<html><header><title>我的私货</title></header><body><ul>%s</ul></body></html>"
+
+         urls = list()
+         for file in glob.glob(os.path.join(file_dir, "*")):
+            urls.append(file.replace(project_path, ""))
+         html = html_tmpl % "\n".join(f"<li><a href='{url}'>"
+                                      f"{os.path.basename(url)}</a></li>" for url in urls)
+         return html
+
